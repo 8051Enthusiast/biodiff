51ffc7ceb84bf254ef3b46c5ab30882c0c25ee31
8051Enthusiast <8051enthusiast@protonmail.com>
2021-06-26 01:52:41 +0200

    use proper effect when clearing screen
    the exact behaviour depends on the terminal, but for some terminals,
    the screen would be cleared with reverse video (so the outside area
    would be white). One example of a terminal emulator
    that behaves like this is kitty.

--- src/backend.rs
+++ src/backend.rs
@@ -359,7 +359,10 @@ impl Backend for Cross {
     }
 
     fn clear(&mut self) {
-        queue!(self.buffer, terminal::Clear(terminal::ClearType::All),)
+        self.prev_effect = Some(Attribute::NoReverse);
+        queue!(self.buffer, 
+            style::SetAttribute(Attribute::NoReverse),
+            terminal::Clear(terminal::ClearType::All),)
             .unwrap_or_else(quit_with_error("Could not clear screen"))
     }
 }
