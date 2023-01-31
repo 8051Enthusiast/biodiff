9983428c0f030a992ffdab9b34d49756478a93cb
8051Enthusiast <8051enthusiast@protonmail.com>
2023-01-31 12:12:08 +0100

    print version for -v and --version

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,31 @@ mod search;
 mod util;
 mod view;
 use std::env;
+use std::ffi::OsString;
 use std::process::exit;
 
 use file::FileState;
+
+fn print_usage(name: &OsString) -> ! {
+    eprintln!("usage: {} [file1] [file2]", name.to_string_lossy());
+    exit(1)
+}
 
 fn main() {
     let args: Vec<_> = env::args_os().collect();
     // we expect exactly two arguments, being the files
     // might extend this in the future, but for now this is enough
     let (xfile, yfile) = match &args[1..] {
-        [a, b] => (a, b),
-        _otherwise => {
-            eprintln!("usage: {} [file1] [file2]", args[0].to_string_lossy());
-            exit(1);
+        [s] => {
+            if matches!(s.to_str(), Some("-v" | "--version")) {
+                println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+                exit(0);
+            } else {
+                print_usage(&args[0])
+            }
         }
+        [a, b] => (a, b),
+        _otherwise => print_usage(&args[0]),
     };
     let x = FileState::from_file(xfile).unwrap_or_else(|e| {
         eprintln!("Could not read {}: {}", xfile.to_string_lossy(), e);
