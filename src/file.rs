9e800b5060de961c4683cc833bdec7b45415d48f
8051Enthusiast <8051enthusiast@protonmail.com>
2023-07-28 18:26:38 +0200

    remove mmap
    biodiff already falls back to reading the entire file into memory anyway
    if the mmap call fails. The memory usage from the alignment
    algorithms is fairly high anyway, so the pros of not using mmap
    (consistent view of files at one time, no locking) probably outweights
    the cons (slightly larger startup time on larger files).

--- src/file.rs
+++ src/file.rs
@@ -1,29 +1,8 @@
-use memmap2::{Mmap, MmapOptions};
-use std::{ffi::OsStr, fs::File, io::Read, ops::Deref, sync::Arc};
+use std::{ffi::OsStr, fs::File, io::Read, sync::Arc};
 
 use crate::{search::SearchResults, util::ilog2};
 
-pub type FileContent = Arc<MaybeMapped>;
-
-#[derive(Debug)]
-/// file that is either mmap'd or loaded into memory.
-/// It implements an deref onto [u8] so that it can be
-/// read as a slice.
-pub enum MaybeMapped {
-    Mapped(Mmap),
-    Vector(Vec<u8>),
-}
-
-impl Deref for MaybeMapped {
-    type Target = [u8];
-
-    fn deref(&self) -> &Self::Target {
-        match self {
-            MaybeMapped::Mapped(m) => m,
-            MaybeMapped::Vector(v) => v,
-        }
-    }
-}
+pub type FileContent = Arc<Vec<u8>>;
 
 /// The bytes of a file along with its filename and an index pointing at a byte of the file
 #[derive(Debug)]
@@ -38,16 +17,15 @@ impl FileState {
     /// Reads a PointedFile from a path, with index 0.
     pub fn from_file(name: &OsStr) -> Result<Self, std::io::Error> {
         let mut file = File::open(name)?;
-        let content = unsafe { MmapOptions::new().map(&file) }
-            .map(MaybeMapped::Mapped)
-            .or_else(|_| {
-                let mut vec = Vec::new();
-                file.read_to_end(&mut vec).map(|_| MaybeMapped::Vector(vec))
-            })?;
-        let f = Arc::new(content);
+        // while the filesize might change between the metadata call and the read_to_end call,
+        // in most cases it will make sure that the vec does not have too much capacity
+        let filesize = file.metadata()?.len() as usize;
+        let mut vec = Vec::with_capacity(filesize);
+        file.read_to_end(&mut vec)?;
+        let content = Arc::new(vec);
         Ok(FileState {
             name: name.to_string_lossy().to_string(),
-            content: f,
+            content,
             index: 0,
             search: None,
         })
