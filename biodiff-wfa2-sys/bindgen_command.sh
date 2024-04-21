cb21b3fc2cd00c370f9ca5a11266e8ffc137ae4e
8051Enthusiast <8051enthusiast@protonmail.com>
2024-04-21 07:03:57 +0200

    Revert "don't include system defs"
    This reverts commit d7bb6852f0feee169f7bba2a5917d0d20d683031.
    
    This does not compile because bindgen does not generate Copy/Clone
    traits.

--- biodiff-wfa2-sys/bindgen_command.sh
+++ biodiff-wfa2-sys/bindgen_command.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
-bindgen --allowlist-function 'wavefront_.*' \
-        --allowlist-var 'wavefront_.*' \
-        --blocklist-type 'timespec' \
-        --blocklist-type 'FILE' \
-        --blocklist-type '_IO_.+' \
-        --raw-line '#![allow(non_upper_case_globals,non_snake_case,non_camel_case_types,unused)]' \
-        --raw-line 'pub type FILE = libc::FILE;' \
-        --raw-line 'pub type timespec = libc::timespec;' \
-        WFA2-lib/wavefront/wavefront_align.h -o src/lib.rs \
-        -- -IWFA2-lib
+bindgen --allowlist-function 'wavefront_.*' --allowlist-var 'wavefront_.*' WFA2-lib/wavefront/wavefront_align.h -o src/bindings.rs -- -IWFA2-lib
